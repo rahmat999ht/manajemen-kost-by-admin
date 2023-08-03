6e872c351f1099ec07bd89bc1166e4aa7b3d1dc6
yayat_99 <rahmat999ht@gmail.com>
2023-08-03 18:19:38 +0800

    update profil screen

--- lib/presentation/profil/profil.screen.dart
+++ lib/presentation/profil/profil.screen.dart
@@ -46,81 +46,79 @@ class StreamProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizeApp.h60,
-                isStream
-                    ? HeaderProfile(
-                        foto: admin?.foto,
-                        nama: admin?.nama,
-                        status: admin?.status,
-                      )
-                    : const SizedBox(
-                        height: 167,
-                        width: 167,
-                        child: LoadingState(),
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizeApp.h60,
+              isStream
+                  ? HeaderProfile(
+                      foto: admin?.foto,
+                      nama: admin?.nama,
+                      status: admin?.status,
+                    )
+                  : const SizedBox(
+                      height: 147,
+                      width: 147,
+                      child: LoadingState(),
+                    ),
+              SizeApp.h20,
+              const Divider(
+                height: 5,
+                thickness: 1.5,
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const ScrollPhysics(),
+                itemCount: c.listItem.length,
+                itemBuilder: (context, index) {
+                  List<void Function()> listTapItem = [
+                    () {
+                      c.tapPersonalDetails(admin!);
+                    },
+                    c.tapPemasukan,
+                    c.tapPengeluaran,
+                  ];
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      CardProfile(
+                        onTap: listTapItem[index],
+                        title: c.listItem[index],
+                        icon: c.listAssetsItem[index],
                       ),
-                SizeApp.h30,
-                const Divider(
-                  height: 10,
-                  thickness: 1.5,
-                ),
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: const ScrollPhysics(),
-                  itemCount: c.listItem.length,
-                  itemBuilder: (context, index) {
-                    List<void Function()> listTapItem = [
-                      () {
-                        c.tapPersonalDetails(admin!);
-                      },
-                      c.tapPemasukan,
-                      c.tapPengeluaran,
-                    ];
-                    return Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        CardProfile(
-                          onTap: listTapItem[index],
-                          title: c.listItem[index],
-                          icon: c.listAssetsItem[index],
-                        ),
-                        SizeApp.h20,
-                      ],
-                    );
-                  },
-                ),
-                SizeApp.h10,
-                const Divider(
-                  height: 10,
-                  thickness: 1.5,
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                CardProfile(
-                  onTap: c.tapTentang,
-                  title: 'Tentang',
-                  icon: Assets.tentang,
-                ),
-                SizeApp.h20,
-                ButtonPrymary(
-                  onPressed: c.alertLogOut,
-                  text: 'Log-out',
-                ),
-                SizeApp.h16,
-              ],
-            ),
-          ],
-        ),
+                      SizeApp.h20,
+                    ],
+                  );
+                },
+              ),
+              SizeApp.h8,
+              const Divider(
+                height: 5,
+                thickness: 1.5,
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              CardProfile(
+                onTap: c.tapTentang,
+                title: 'Tentang',
+                icon: Assets.tentang,
+              ),
+              SizeApp.h20,
+              ButtonPrymary(
+                onPressed: c.alertLogOut,
+                text: 'Log-out',
+              ),
+              SizeApp.h16,
+            ],
+          ),
+        ],
       ),
     );
   }
